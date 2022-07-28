6b9beb88882c445f0851052e944b1eb6a69e9a70
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-07-28 02:22:42 +0200

    init

---
+++ src/main/java/com/vv/controller/GreetController.java
@@ -0,0 +1,13 @@
+package com.vv.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hi!! there...";
+    }
+}
